bf1e2ad0deaaa9a02b1219b3aff0e802842b70a9
john castrillon <Jcastrillon87@yahoo.com>
2016-10-11 16:47:18 -0400

    add authentication function, authcontroller, and an authrouter

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const session = require('express-session');
+const authentication = require('../api/middleware/authentication.js');
+const authRouter = require('../api/routes/authRouter.js');
 
 const app = express();
 
@@ -18,5 +20,10 @@ app.use(session({
 }));
 
 app.use(morgan('dev'));
+
+app.use('/test', authentication);
+app.use('/test', authRouter);
+app.use('/test/users', userRouter);
+app.use('/test/trains', trainsRouter);
 
 module.exports = app;

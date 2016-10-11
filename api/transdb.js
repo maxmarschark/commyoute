5dfe019e329ffb26ffaa8de90b414ab389de69ab
Wesley Waterman <wwaterman12@gmail.com>
2016-10-11 11:53:34 -0400

    remove package.json and transdb.js

--- api/transdb.js
+++
@@ -1,9 +0,0 @@
-const pgPromise = require('pg-promise');
-
-const pg = pgPromise();
-
-const databaseURL = 'postgres://wesleywaterman:@localhost:5432/funstuff';
-
-const db = pg(databaseURL);
-
-module.exports = db;

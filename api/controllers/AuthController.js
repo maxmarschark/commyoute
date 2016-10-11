b9dbbea771732b2c7ea182307d68b11579e4042c
john castrillon <Jcastrillon87@yahoo.com>
2016-10-11 18:20:33 -0400

    add a static login function to the authcontroller

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,2 +1,25 @@
 const UserDAO = require('../dao/UserDAO');
-const createToken = require()
+const createToken = require('..utils/createToken');
+const bcrypt = require('bcrypt');
+
+class AuthController {
+  static login(req, res) {
+    const { email, password } = req.body;
+    UserDAO.findBy({ email })
+           .then((user) => {
+             if (!bcrypt.compareSync(password, user.password)) {
+               res.status(401).end();
+             } else {
+               req.session.currentUser = user;
+               const token = createToken(user);
+               res.cookie('token', token);
+               res.status(200).json(user);
+             }
+           })
+           .catch((err) => {
+             console.error(err);
+             res.status(401).end();
+           });
+  }
+  
+}

149d34c759342d252d41b627fa14448811413387
Wesley Waterman <wwaterman12@gmail.com>
2016-10-11 11:00:26 -0400

    update schema and seed

--- api/schema.sql
+++ api/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS transportation;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  u_id SERIAL PRIMARY KEY,
   firstname VARCHAR(255),
   lastname VARCHAR(255),
   email VARCHAR(255),
@@ -10,13 +10,13 @@ CREATE TABLE users (
 );
 
 CREATE TABLE trans (
-  id SERIAL PRIMARY KEY,
+  t_id SERIAL PRIMARY KEY,
   type VARCHAR(255),
   name VARCHAR(255),
   overall INTEGER,
   delay BOOLEAN,
   crowded BOOLEAN,
   smelly BOOLEAN,
-  post_time TIMESTAMP,
-  user_id integer
+  review_Date datetime NOT NULL DEFAULT GETDATE(),
+  u_id INTEGER FOREIGN KEY REFERENCES users(u_Id)
 );
--- api/seed.sql
+++ api/seed.sql
@@ -5,4 +5,10 @@ ALTER SEQUENCE users_id_seq RESTART WITH 1;
 ALTER SEQUENCE trans_id_seq RESTART WITH 1;
 
 INSERT INTO users(firstname, lastname, email, password) VALUES ('Wes', 'Waterman', 'wes@wes.com', 'password');
-INSERT INTO users(firstname, lastname, email, password) VALUES ('Max', 'Marshark', 'wes@wes.com', 'password');
+INSERT INTO users(firstname, lastname, email, password) VALUES ('Max', 'Marshark', 'max@max.com', 'password');
+INSERT INTO users(firstname, lastname, email, password) VALUES ('Sam', 'Peterson', 'sam@peterson.com', 'password');
+INSERT INTO users(firstname, lastname, email, password) VALUES ('John', 'Castrillon', 'john@john.com', 'password');
+
+-- 7 train
+INSERT INTO trans(type, name, overall, delay, crowded, smelly, post_time, user_id) VALUES ('Train', '7', 2, true, true, false, )
+

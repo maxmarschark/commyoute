3676c03daf831147101875f347da21c2de4a74c1
Wesley Waterman <wwaterman12@gmail.com>
2016-10-11 11:35:28 -0400

    fix seed timestamp

--- api/seed.sql
+++ api/seed.sql
@@ -1,9 +1,6 @@
 TRUNCATE TABLE users;
 TRUNCATE TABLE trans;
 
-ALTER SEQUENCE users_id_seq RESTART WITH 1;
-ALTER SEQUENCE trans_id_seq RESTART WITH 1;
-
 INSERT INTO users(firstname, lastname, email, password) VALUES ('Wes', 'Waterman', 'wes@wes.com', 'password');
 INSERT INTO users(firstname, lastname, email, password) VALUES ('Max', 'Marshark', 'max@max.com', 'password');
 INSERT INTO users(firstname, lastname, email, password) VALUES ('Sam', 'Peterson', 'sam@peterson.com', 'password');

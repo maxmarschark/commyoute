60968f6eb0493662d76c8b1755f604f9c1845f00
Wesley Waterman <wwaterman12@gmail.com>
2016-10-11 17:16:29 -0400

    add find user, create, and delete to userDAO

--- api/dao/userDAO.js
+++ api/dao/userDAO.js
@@ -5,5 +5,18 @@ class UserDAO {
   static all() {
     return db.map(sql.all, [], (row) => new User(row));
   }
-
+  static findUser(keyValue) {
+    const key = Object.keys(keyValue)[0];
+    const value = keyValue[key];
+    return db.one(sql.find, [key, value])
+             .then((row) => new User(row));
+  }
+  static create({ email, password }) {
+    return db.one(sql.create, [email, password])
+             .then((row) => new User(row));
+  }
+  static delete(id) {
+    return db.none(sql.delete, [id]);
+  }
+}
 }

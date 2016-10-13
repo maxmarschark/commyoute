224b5afcac07472aec4163fbd27e80d5e6b14490
john castrillon <Jcastrillon87@yahoo.com>
2016-10-13 15:38:25 -0400

    changed jsx names to fit react standards which are capital first letters.

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -31,7 +31,7 @@ class Home extends Component {
               <TrainIcon
               img_url={train.img_url}
               />
-            )
+            );
           })
         }
       </div>

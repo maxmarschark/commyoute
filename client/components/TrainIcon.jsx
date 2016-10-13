540b1e6e3a475f864c0a1a1965ea5bd4623e2009
Sam Peterson <samuel.e.peterson@gmail.com>
2016-10-13 16:27:11 -0400

    add working render of train icons

--- client/components/TrainIcon.jsx
+++ client/components/TrainIcon.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class TrainIcon extends Component {
   constructor(props) {
     super(props);
+    this.img = this.props.url;
+    this.name = this.props.trainName;
   }
   render() {
     return (
-      <div>
+      <div className="trainIcon" id={this.name} >
         <img
-          src={this.props.img_url}
+          src={this.img}
           className="train"
           height="50"
           width="50"

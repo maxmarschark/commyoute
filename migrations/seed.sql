12ddee3c246b003ee462ea0b37e82147090f8f05
john castrillon <Jcastrillon87@yahoo.com>
2016-10-13 14:53:28 -0400

    temporarily removed trains

--- migrations/seed.sql
+++ migrations/seed.sql
@@ -5,15 +5,3 @@ INSERT INTO users(name, email, password) VALUES ('Wes Waterman', 'wes@wes.com', 
 INSERT INTO users(name, email, password) VALUES ('Max Marshark', 'max@max.com', 'password');
 INSERT INTO users(name, email, password) VALUES ('Sam Peterson', 'sam@peterson.com', 'password');
 INSERT INTO users(name, email, password) VALUES ('John Castrillon', 'john@john.com', 'password');
-
--- 1 train
-INSERT INTO trans(type, name, overall, delay, crowded, smelly, img_url, user_id) VALUES ('Train', '1', 2, 1, 1, 0, '', 1);
-IN
-
---2 train
-INSERT INTO trans(type, name, overall, delay, crowded, smelly, img_url, user_id) VALUES ('Train', '2', 2, 1, 1, 0, '', 1);
-IN
-
--- 3 train
-INSERT INTO trans(type, name, overall, delay, crowded, smelly, img_url, user_id) VALUES ('Train', '3', 2, 1, 1, 0, '', 1);
-IN

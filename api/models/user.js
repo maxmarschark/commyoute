9197c4e97cb550efd82af6e1667e019e23ead9f6
john castrillon <Jcastrillon87@yahoo.com>
2016-10-13 21:25:09 -0400

    get user id

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,6 @@
 class User {
-  constructor({ name, email, password }) {
+  constructor({ id, name, email, password }) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
